5fcd31505f1f51f317332e3fdef4cf949e137132
Andreas <andreas.basso@std.heh.be>
2020-04-28 21:22:12 +0200

    correction bug inscription

--- inscription.php
+++ inscription.php
@@ -1,23 +1,23 @@
 <!DOCTYPE html>
 <html lang = "fr">
 	<head>
-			<?php 
+			<?php
                require_once('head.php');
             ?>
 	</head>
 
 	<body>
 
-           <?php 
+           <?php
                require_once('connexion.php');
-               
+
             ?>
 
-         <nav id="navtop">	
+         <nav id="navtop">
 			<a href="index.php"><img class="logo" src = "img/Logo_Newflix.png" alt = "Logo officiel du site"></a>
-            
-			<a href="login.php" class="login" >Connexion</a>    
-            <a href="inscription.php">Inscription</a>	
+
+			<a href="login.php" class="login" >Connexion</a>
+            <a href="inscription.php">Inscription</a>
 		</nav>
 
 
@@ -27,7 +27,7 @@
 
 			$req=$bd->prepare('SELECT * FROM users');
 			$req->execute();
-			while ($result=$req->fetch(PDO::FETCH_OBJ) ) 
+			while ($result=$req->fetch(PDO::FETCH_OBJ) )
 			{
 				$users_t[] = $result;
 			}
@@ -35,72 +35,77 @@
 
 
 
-		if (isset($_REQUEST['name'], $_REQUEST['email'], $_REQUEST['password'], $_REQUEST['prenom'])){
-		  
-		  // récupérer le nom de l'utilisateur 
+		if (isset($_REQUEST['name'],$_REQUEST['prenom'], $_REQUEST['email'], $_REQUEST['password'] ))
+		{
+
+		  // récupérer le nom de l'utilisateur
 		  $name    = $_REQUEST['name'];
 
-		  // récupérer le prénom utilisateur 
+		  // récupérer le prénom utilisateur
 		  $prenom   = $_REQUEST['prenom'];
-		  
+
 		  // récupérer l'email
-		  	$email = $_REQUEST['email']; 
-		  
+		  	$email = $_REQUEST['email'];
+
 
 		  // récupérer le mot de passe et le hasher
 		  $mdp = $_REQUEST['password'] ;
 		  $password = password_hash($mdp, PASSWORD_DEFAULT);
-		  
 
-		  if (in_array ($email,$users_t)) {
-		  	$email = $_REQUEST['email']; 
+
+		  if (in_array ($email,$users_t))
+			{
+		  	$email = $_REQUEST['email'];
 		  }
-		  else {
+		  else
+			{
 		  	echo "Un compte a déjà été crée avec cette adresse mail";
 		  }
 
 
 		  $bd->exec( "INSERT into `users` (Adresse_mail, Nom, Prenom, Mot_de_passe)
-		        VALUES ('$name', '$email','$prenom' , '$password')" ) ;
-		 
-		    if($bd){
+		        VALUES ('$email','$name','$prenom' , '$password')" ) ;
+
+		    if($bd)
+				{
 		       echo "<div class='sucess'>
 		             <h3>Vous êtes inscrit avec succès.</h3>
 		             <p>Cliquez ici pour vous <a href='login.php'>connecter</a></p>
 		       </div>";
 		    }
-		}else{
+		}else
+		{
 		?>
 
 
 				<form class="box" action="" method="post">
-				  
+
 				    <h1 class="box-title">S'inscrire</h1>
-				  <input type="text" class="box-input" name="name" 
+				  <input type="text" class="box-input" name="name"
 				  placeholder="Nom " required />
 
-				  <input type="text" class="box-input" name="prenom" 
+				  <input type="text" class="box-input" name="prenom"
 				  placeholder="Prénom" required />
-				  
-				    <input type="text" class="box-input" name="email" 
+
+				    <input type="text" class="box-input" name="email"
 				  placeholder="Email" required />
-				  
-				    <input type="password" class="box-input" name="password" 
+
+				    <input type="password" class="box-input" name="password"
 				  placeholder="Mot de passe" required />
 
-				  <input type="password" class="box-input" name="password1" 
+				  <input type="password" class="box-input" name="password1"
 				  placeholder="Confirmation du mot de passe" required />
-				  
-				    <input type="submit" name="submit" 
+
+				    <input type="submit" name="submit"
 				  value="S'inscrire" class="box-button" />
-				  
-				    <p class="box-register">Déjà inscrit? 
+
+				    <p class="box-register">Déjà inscrit?
 				  <a href="login.php">Connectez-vous ici</a></p>
 				</form>
 
-			
+
 		<?php } ?>
 
-		
+
 	</body>
-</html>	
+</html>
